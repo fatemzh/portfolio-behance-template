abaeb5f41e81c45f27423d0327ad68ca699f0d81
fatemzh <abid.fatem@hotmail.fr>
2023-07-04 18:23:34 +0200

    v1.3.abtnepageDONE

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('.header');
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
