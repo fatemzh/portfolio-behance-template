7f915a8c7eeb419a8ef39939bf2cc2632fa38ac7
fatemzh <abid.fatem@hotmail.fr>
2023-07-06 20:48:13 +0200

    javascript4servicesandarchivesDONE

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,11 @@
+/*CODE AFFICHER PLUS AVEC LE "+" POUR LA PAGE SERVICES */ 
 window.addEventListener('load', function() {
-    const toggle = document.querySelector('.toggle');
-    const content = document.querySelector('.content');
-  
+  const toggleList = document.querySelectorAll('.toggle');
+
+  toggleList.forEach(function(toggle) {
     toggle.addEventListener('click', function() {
+      const content = toggle.parentElement.nextElementSibling;
+
       if (content.style.display === 'none') {
         content.style.display = 'block';
         toggle.textContent = '-';
@@ -12,4 +15,16 @@ window.addEventListener('load', function() {
       }
     });
   });
-  
+});
+
+/*CODE AFFICHER VOIR PLUS POUR LA PAGE ARCHIVES */ 
+
+window.addEventListener('load', function() {
+  const seeMoreButton = document.getElementById('see-more');
+  const hiddenCards = document.getElementById('hidden-cards');
+
+  seeMoreButton.addEventListener('click', function() {
+    hiddenCards.style.display = 'block';
+    seeMoreButton.style.display = 'none';
+  });
+});
